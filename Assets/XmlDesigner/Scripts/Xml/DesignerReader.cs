38d0057c7b73661d93105cd55196763774b2b5a5
misakiMeiii <724886365@qq.com>
2024-01-15 14:56:41 +0800

    v0.1.0 新增读取设计的方法

---
+++ Assets/XmlDesigner/Scripts/Xml/DesignerReader.cs
@@ -0,0 +1,103 @@
+﻿using System.Xml;
+using UnityEngine;
+
+namespace XmlDesigner
+{
+    public static class DesignerReader
+    {
+        public static void ReadDesignFile(string filePath)
+        {
+            var doc = new XmlDocument();
+            doc.Load(filePath);
+            var rootNode = doc.SelectSingleNode("RootClass");
+            if (rootNode == null)
+            {
+                Debug.LogError("读取错误，文件根节点必须为RootClass");
+            }
+            else
+            {
+                var rootElement = new RootElement();
+                foreach (XmlNode node in rootNode)
+                {
+                    switch (node.Name)
+                    {
+                        case "Name":
+                            rootElement.Name = node.InnerText;
+                            break;
+                        case "NameSpace":
+                            rootElement.NameSpace = node.InnerText;
+                            break;
+                        case "ChildElement":
+                            rootElement.ChildElements.Add(node.CreateChildElement());
+                            break;
+                        case "CustomElement":
+                            rootElement.CustomElements.Add(node.CreateCustomElement());
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static CustomElement CreateCustomElement(this XmlNode node)
+        {
+            var customElement = new CustomElement();
+
+            if (node.Attributes?["name"] != null)
+            {
+                customElement.Name = node.Attributes["name"].Value;
+            }
+
+            foreach (XmlNode childNode in node)
+            {
+                if (childNode.Name == "ChildElement")
+                {
+                    customElement.ChildElements.Add(childNode.CreateChildElement());
+                }
+            }
+
+            return customElement;
+        }
+
+        private static ChildElement CreateChildElement(this XmlNode node)
+        {
+            var childElement = new ChildElement();
+
+            if (node.Attributes?["name"] != null)
+            {
+                childElement.Name = node.Attributes["name"].Value;
+            }
+
+            if (node.Attributes?["isAttribute"] != null)
+            {
+                childElement.IsAttribute = node.Attributes["isAttribute"].Value.ToBool();
+            }
+
+            if (node.Attributes?["elementType"] != null)
+            {
+                childElement.ElementType = node.Attributes["elementType"].Value.ToEnum<ElementType>();
+            }
+
+            if (node.Attributes?["referenceType"] != null)
+            {
+                childElement.ReferenceType = node.Attributes["referenceType"].Value.ToEnum<BaseType>();
+            }
+
+            if (node.Attributes?["keyType"] != null)
+            {
+                childElement.KeyType = node.Attributes["keyType"].Value.ToEnum<KeyType>();
+            }
+
+            if (node.Attributes?["customType"] != null)
+            {
+                childElement.CustomType = node.Attributes["customType"].Value.ToInt();
+            }
+
+            if (node.Attributes?["defaultValue"] != null)
+            {
+                childElement.DefaultValue = node.Attributes["defaultValue"].Value;
+            }
+
+            return childElement;
+        }
+    }
+}
